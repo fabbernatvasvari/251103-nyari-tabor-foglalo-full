253ed3c4dad76b09c12a046fc9bb8742b42d98fb
fabbernatvasvari <sze4fabber@vasvari.org>
2025-11-06 14:36:56 +0100

    finished model réteg

--- backend/net/MyConsoleApp/Models/Camp.cs
+++ backend/net/MyConsoleApp/Models/Camp.cs
@@ -8,5 +8,8 @@ namespace ConsoleApp1.Shared.Models
 {
     internal class Camp
     {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public Organizer organizer { get; set; }
     }
 }
